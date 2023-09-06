9a06c9c1e3b7a5b3528ea4f8dc311ecb1dd83c73
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-09-06 14:32:17 +0300

    feat: enable skew correction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2176,7 +2176,7 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-//#define SKEW_CORRECTION
+#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
@@ -2201,7 +2201,7 @@
   #endif
 
   // Enable this option for M852 to set skew at runtime
-  //#define SKEW_CORRECTION_GCODE
+  #define SKEW_CORRECTION_GCODE
 #endif
 
 //=============================================================================
