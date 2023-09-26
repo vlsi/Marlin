929f9be8d091f4f2a62250ba6989e0a8a6588a56
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-09-26 10:23:47 +0300

    TMC_HOME_PHASE

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3352,7 +3352,7 @@
    *
    * Values from 0..1023, -1 to disable homing phase for that axis.
    */
-   #define TMC_HOME_PHASE { 896, 896, 896 }
+   #define TMC_HOME_PHASE { -1, -1, 896 }
 
   /**
    * Step on both rising and falling edge signals (as with a square wave).
