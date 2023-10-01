e669c3d3bab403526bd674044625da95cb344fa7
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-10-01 23:09:18 +0300

    feat: fix environment names for AutoBuildMarlin

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -699,6 +699,8 @@
   #include "stm32f4/pins_MKS_ROBIN_NANO_V3.h"   // STM32F4                                env:mks_robin_nano_v3 env:mks_robin_nano_v3_usb_flash_drive env:mks_robin_nano_v3_usb_flash_drive_msc
 #elif MB(MKS_ROBIN_NANO_V3_1)
   #include "stm32f4/pins_MKS_ROBIN_NANO_V3.h"   // STM32F4                                env:mks_robin_nano_v3_1 env:mks_robin_nano_v3_1_usb_flash_drive env:mks_robin_nano_v3_1_usb_flash_drive_msc
+#elif MB(MKS_NANO6)
+  #include "stm32f4/pins_MKS_ROBIN_NANO6.h"     // STM32F4                                env:reborn2_manual env:reborn2_inductive_zmin env:reborn2_inductive_runout2 env:reborn2_bltouch_zmin env:reborn2_bltouch_runout2
 #elif MB(ANET_ET4)
   #include "stm32f4/pins_ANET_ET4.h"            // STM32F4                                env:Anet_ET4_no_bootloader env:Anet_ET4_OpenBLT
 #elif MB(ANET_ET4P)
@@ -729,8 +731,6 @@
   #include "stm32f4/pins_MKS_SKIPR_V1_0.h"      // STM32F4                                env:mks_skipr_v1 env:mks_skipr_v1_nobootloader
 #elif MB(TRONXY_V10)
   #include "stm32f4/pins_TRONXY_V10.h"          // STM32F4                                env:STM32F446_tronxy
-#elif MB(MKS_NANO6)
-  #include "stm32f4/pins_MKS_ROBIN_NANO6.h"           // STM32F4                          env:mks_robin_nano6
 
 //
 // ARM Cortex M7
